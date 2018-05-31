dae6e61f4f79200265f43ef4f0da51af9fbb97c4
Richard Hriech <hardric501@gmail.com>
2018-05-31 15:09:45 +0200

    endtext, starttext

--- src/com/codecool/termlib/GameLoop.java
+++ src/com/codecool/termlib/GameLoop.java
@@ -2,6 +2,7 @@ package com.codecool.termlib;
 
 import com.codecool.game.*;
 
+import java.io.*;
 import java.rmi.UnexpectedException;
 
 class GameLoop {
@@ -9,7 +10,9 @@ class GameLoop {
     // Mezi run cmd
     // " java -classpath /mnt/e/CC/OOP/terminal-library/out/production/terminal-library/com/codecool/termlib/GameLoop "
     
+    
     public static void main(String[] args) {
+        
         
         int[][] map = MapReader.mapReader();
         Player player;
@@ -18,30 +21,22 @@ class GameLoop {
         } catch (UnexpectedException e) {
             System.out.print(e);
             player = new Player(new Coordinate(1, 1));
-            
         }
         
         try {
-             /*//WHILE
-                  // TODO: RESET
-                  // TODO: DRAWING
-                  // TODO: INPUT HANDLING
-                  // TODO: TIMER
-                  */
             boolean endGame = false;
             
             Terminal.runTerminalCommand(TerminalCommands.SET_TERMINAL_RAW);
             Terminal.runTerminalCommand(TerminalCommands.HIDE_CURSOR);
+            String startText = "EPIC LABYRINTH";
+            Terminal.toiletPrint(startText);
+            Thread.sleep(1000);
             Terminal.runTerminalCommand(TerminalCommands.CLEAR_TERMINAL);
             Terminal.drawWholeArray(map);
             Terminal.moveCursorTo(player.getCoordinates().getxPos() + 1, player.getCoordinates().getyPos() + 1);
             Terminal.runTerminalCommand(TerminalCommands.SAVE_CURSOR_POSITION);
             player.drawPlayer();
-            
             while (!endGame) {
-                // Terminal.drawWholeArray(map);
-                // player.drawPlayer();
-                
                 if (System.in.available() != 0) {
                     int c = System.in.read();
                     if (c == '0') {
@@ -64,14 +59,24 @@ class GameLoop {
                 }
                 Thread.sleep(64);
             }
-            
             Terminal.runTerminalCommand(TerminalCommands.COLOR_BG_DEFAULT);
             Terminal.runTerminalCommand(TerminalCommands.CLEAR_TERMINAL);
+            
+            for (int i = 0; i < 3; ++i) {
+                String endText = "YOU WIN";
+                Terminal.toiletPrint(endText);
+                Thread.sleep(500);
+                Terminal.runTerminalCommand(TerminalCommands.CLEAR_TERMINAL);
+                String endText2 = "PREORDER NOW ON STEAM";
+                Terminal.toiletPrint(endText2);
+                Thread.sleep(1000);
+            }
             Terminal.runTerminalCommand(TerminalCommands.SET_TERMINAL_DEFAULT);
             Terminal.runTerminalCommand(TerminalCommands.SHOW_CURSOR);
-            
         } catch (Exception e) {
             System.out.println(e);
         }
     }
+    
+    
 }
